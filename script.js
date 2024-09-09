4109b2d85193504bee6c20cbc971769110ba139d
Dual-AlexKey <almissann@gmail.com>
2024-09-09 13:30:01 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const errorMessage = document.getElementById('error-message');
+const modalGameOver = document.getElementById('gameOverModal');
+const hearts = document.querySelectorAll('.heart');
 const cards = document.querySelectorAll('.card');
+const reintentarBtn = document.getElementById('reintentarBtn');
+const salirBtn = document.getElementById('salirBtn');
+
+const audioIncorrecto = new Audio('/audio/incorrecto.mp3');
 let firstCard = null;
 let secondCard = null;
 let lockBoard = false;
+
+let selectedCard = null;
 let matchedPairs = 0;
 let lives = 3; // Número inicial de vidas
+let attempts = 0;
+const maxAttempts = 3;
 
 const livesDisplay = document.getElementById('lives');
 
@@ -47,19 +59,56 @@ function unflipCards() {
         secondCard.classList.add('hidden');
         secondCard.classList.remove('revealed');
         resetBoard();
-        updateLives(-1); // Descontar una vida
+        attempts++;
+        showMessage(errorMessage, audioIncorrecto);
+            lives--;
+        if (lives >= 0) {
+            animateHeartDisappearance(hearts[lives]);
+        }
+
+        if (attempts === maxAttempts) {
+            setTimeout(mostrarGameOver, 500);
+            lockBoard = true; // Deshabilitar el juego
+        cards.forEach(card => card.removeEventListener('click', flipCard)); // Desactivar eventos de clic  
+        }
+        
+        
     }, 1000);
 }
 
-function updateLives(change) {
-    lives += change;
-    livesDisplay.textContent = lives;
+reintentarBtn.addEventListener('click', () => window.location.reload());
+    salirBtn.addEventListener('click', () => window.location.href = '../index.html');
 
-    if (lives <= 0) {
-        document.getElementById('message').textContent = '¡Perdiste! No tienes más vidas.';
-        lockBoard = true; // Deshabilitar el juego
-        cards.forEach(card => card.removeEventListener('click', flipCard)); // Desactivar eventos de clic
-    }
+
+function mostrarGameOver() {
+    modalGameOver.style.display = 'block';
+    audioGameOver.play();
+}
+function toggleSound() {
+    audio.volume = isMuted ? 0.03 : 0;
+    isMuted = !isMuted;
+    soundIcon.style.display = isMuted ? 'none' : 'block';
+    muteIcon.style.display = isMuted ? 'block' : 'none';
+}
+
+function animateHeartDisappearance(heart) {
+    heart.style.transition = 'opacity 0.5s ease-out';
+    heart.style.opacity = '0';
+    setTimeout(() => {
+        heart.style.display = 'none';
+    }, 500);
+}
+function showMessage(messageElement, audio) {
+    messageElement.style.display = 'block';
+    playAudio(audio);
+    setTimeout(() => {
+        messageElement.style.display = 'none';
+    }, 1000);
+}
+
+function playAudio(audio) {
+    audio.currentTime = 0;
+    audio.play();
 }
 
 function resetBoard() {
@@ -80,3 +129,4 @@ function shuffleCards() {
 // Initialize game
 shuffleCards();
 cards.forEach(card => card.addEventListener('click', flipCard));
+});
