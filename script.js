9ca4ee44b78b5ada15283c88690554788b35838d
Dual-AlexKey <almissann@gmail.com>
2024-09-10 14:48:06 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
     const errorMessage = document.getElementById('error-message');
-const modalGameOver = document.getElementById('gameOverModal');
-const hearts = document.querySelectorAll('.heart');
-const cards = document.querySelectorAll('.card');
-const reintentarBtn = document.getElementById('reintentarBtn');
-const salirBtn = document.getElementById('salirBtn');
+    const correctMessage = document.getElementById('correct-message');
+    const modalGameOver = document.getElementById('gameOverModal');
+    const hearts = document.querySelectorAll('.heart');
+    const cards = document.querySelectorAll('.card');
+    const reintentarBtn = document.getElementById('reintentarBtn');
+    const salirBtn = document.getElementById('salirBtn');
+    const completedMessage = document.getElementById('completed');
+    const audio = document.querySelector('#audioElement');
+    const muteIcon = document.querySelector('#mute-icon');
+    const soundIcon = document.querySelector('#sound-icon');
 
-const audioIncorrecto = new Audio('/audio/incorrecto.mp3');
+    const audioCorrecto = new Audio('audio/correcto.mp3');
+    const audioIncorrecto = new Audio("audio/incorrecto.mp3");
+    const audioGameOver = new Audio('audio/gameover.mp3');
+    audio.volume = 0.03;
+    audioGameOver.volume = 0.5;
+
+
+let isMuted = false;
 let firstCard = null;
 let secondCard = null;
 let lockBoard = false;
-
-let selectedCard = null;
 let matchedPairs = 0;
-let lives = 3; // Número inicial de vidas
+let lives = 4; // Número inicial de vidas
 let attempts = 0;
-const maxAttempts = 3;
+const totalPairs = 6;
+const maxAttempts = 4;
 
-const livesDisplay = document.getElementById('lives');
+soundIcon.addEventListener('click', toggleSound);
+muteIcon.addEventListener('click', toggleSound);
 
-function flipCard() {
+cards.forEach(card => {
+    card.addEventListener('click', selectCard);
+});
+
+reintentarBtn.addEventListener('click', () => window.location.reload());
+salirBtn.addEventListener('click', () => window.location.href = '../index.html');
+
+function toggleSound() {
+    audio.volume = isMuted ? 0.03 : 0;
+    isMuted = !isMuted;
+    soundIcon.style.display = isMuted ? 'none' : 'block';
+    muteIcon.style.display = isMuted ? 'block' : 'none';
+}
+
+
+function selectCard() {
     if (lockBoard || this === firstCard || this.classList.contains('matched')) return;
 
     this.classList.remove('hidden');
@@ -39,17 +66,45 @@ function flipCard() {
 function checkForMatch() {
     const isMatch = firstCard.dataset.country === secondCard.dataset.country;
 
-    isMatch ? disableCards() : unflipCards();
+    isMatch ? correctMatch() : unflipCards();
 }
 
-function disableCards() {
+function correctMatch() {
     firstCard.classList.add('matched');
     secondCard.classList.add('matched');
     matchedPairs++;
     resetBoard();
-    if (matchedPairs === 6) {
-        document.getElementById('message').textContent = '¡Ganaste! Has encontrado todas las parejas.';
+    showMessage(correctMessage, audioCorrecto);
+    if (matchedPairs === totalPairs) {
+        completedMessage.style.display = 'block';
     }
+    const allDisabled = Array.from(cards).every(card => card.classList.contains('matched'));
+    
+        if (allDisabled) {
+            // Todas las cartas están deshabilitadas, mostrar la flecha
+            const arrowIcon = document.getElementById('arrow-icon');
+            const modal = document.getElementById("modal");
+            if (modal) {  // Verificación adicional
+                modal.style.display = "flex";
+                modal.classList.add("show");
+                arrowIcon.style.display = 'inline-block';
+                arrowIcon.addEventListener('click', function() {
+                    window.location.href = 'game2.html';
+                });
+    
+                // Ocultar el modal después de 10 segundos
+                setTimeout(() => {
+                    modal.classList.remove("show");
+                    modal.classList.add("hide");
+                    setTimeout(() => {
+                        modal.style.display = "none";
+                        modal.classList.remove("hide");
+                    }, 500); 
+                }, 1000); // 10000 milisegundos = 10 segundos
+            } else {
+                console.error('El elemento con el ID "modal" no existe en el DOM.');
+            }
+        }
 }
 
 function unflipCards() {
@@ -128,5 +183,5 @@ function shuffleCards() {
 
 // Initialize game
 shuffleCards();
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards.forEach(card => card.addEventListener('click', selectCard));
 });
